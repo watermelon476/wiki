80016cfeef9e0b9ca0cd4176db02c35aab767570
liuqu <1776540288@qq.com>
2024-02-16 22:56:23 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/com/example/wiki/controller/TestController.java
+++ src/main/java/com/example/wiki/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.wiki.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,8 +10,13 @@ public class TestController {
      * GET,POST,PUT,DELETE
      * 查询，新增，修改，删除
      */
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello(){
         return "hello world!";
+    }
+
+    @PostMapping("/hello2")
+    public String hello2(String name){
+        return "hello world! post " + name;
     }
 }
