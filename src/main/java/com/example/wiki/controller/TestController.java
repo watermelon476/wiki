76c67646e8d3e4e0a1ab922e2f8404d48e2cba04
liuqu <1776540288@qq.com>
2024-02-17 11:09:50 +0800

    2-8 增加自定义配置项示例

--- src/main/java/com/example/wiki/controller/TestController.java
+++ src/main/java/com/example/wiki/controller/TestController.java
@@ -1,18 +1,23 @@
 package com.example.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
     /**
      * GET,POST,PUT,DELETE
      * 查询，新增，修改，删除
      */
     @GetMapping("/hello")
     public String hello(){
-        return "hello world!";
+        return "hello world! "+ testHello;
     }
 
     @PostMapping("/hello2")
