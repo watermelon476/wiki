b3711e15670472fea3728d70683aa4d439acc5b1
liuqu <1776540288@qq.com>
2024-02-18 22:58:11 +0800

    3-5 集成mybatis generator，生成demo表代码

--- doc/all.sql
+++ doc/all.sql
@@ -8,3 +8,13 @@ create table `test`(
 
 INSERT INTO test (id, name, password)
 VALUES (2, 'lili', '654321');
+
+drop table if exists `demo`;
+create table `demo`(
+                       `id` bigint not null comment 'id',
+                       `name` varchar(50) comment '名称',
+                       primary key (`id`)
+)engine=innodb default character set =utf8mb4 comment ='测试';
+
+INSERT INTO demo (id, name)
+VALUES (2, 'lili');
