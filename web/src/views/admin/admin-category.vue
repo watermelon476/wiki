dc40bcb6fee2c7e97b0aa52dc8744c33a9755d81
liuqu <1776540288@qq.com>
2024-04-09 15:03:12 +0800

    7-5 父分类改为下拉选择

--- web/src/views/admin/admin-category.vue
+++ web/src/views/admin/admin-category.vue
@@ -59,7 +59,19 @@
         <a-input v-model:value="category.name" />
       </a-form-item>
       <a-form-item label="父分类">
-        <a-input v-model:value="category.parent" />
+        <a-select
+            ref="select"
+            v-model:value="category.parent"
+        >
+          <a-select-option value="0">
+            无
+          </a-select-option>
+          <a-select-option v-for="c in level1" :key="c.id" :value="c.id"
+                           :disabled="category.id==c.id">
+            {{c.name}}
+          </a-select-option>
+        </a-select>
+
       </a-form-item>
       <a-form-item label="顺序">
         <a-input v-model:value="category.sort" />
@@ -136,7 +148,6 @@ export default defineComponent({
           message.error(data.message);
         }
       });
-      console.log("111",categories.value)
     };
 
     // --------------------表单------------------------
