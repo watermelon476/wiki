222dc3c2570b9b62d8e705b4d6dd1a7dfa2762f3
liuqu <1776540288@qq.com>
2024-04-09 17:08:53 +0800

    7-6 分类选择改为级联选择组件

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -65,11 +65,12 @@
       <a-form-item label="名称">
         <a-input v-model:value="ebook.name" />
       </a-form-item>
-      <a-form-item label="分类一">
-        <a-input v-model:value="ebook.category1Id" />
-      </a-form-item>
-      <a-form-item label="分类二">
-        <a-input v-model:value="ebook.category2Id" />
+      <a-form-item label="分类">
+        <a-cascader
+            v-model:value="categoryIds"
+            :field-names="{label:'name', value:'id', children:'children'}"
+            :options="level1"
+        />
       </a-form-item>
       <a-form-item label="描述">
         <a-input v-model:value="ebook.description" type="textarea" />
@@ -174,12 +175,18 @@ export default defineComponent({
       });
     };
     // --------------------表单------------------------
-    const ebook = ref({});
+    /**
+     * 数组 [100,101]对应：前端开发 / Vue
+     */
+    const categoryIds = ref();
+    const ebook = ref();
     const modalVisible = ref<boolean>(false);
     const modalLoading = ref<boolean>(false);
 
     const handleModalOk = () => {
       modalLoading.value = true;
+      ebook.value.category1Id = categoryIds.value[0];
+      ebook.value.category2Id = categoryIds.value[1];
       axios.post("/ebook/save",ebook.value).then((response) => {
         modalLoading.value = false;
         const data = response.data; // data = commonResp
@@ -202,6 +209,7 @@ export default defineComponent({
     const edit = (record:any)=>{
       modalVisible.value = true;
       ebook.value = Tool.copy(record);
+      categoryIds.value = [ebook.value.category1Id,ebook.value.category2Id]
     };
     /**
      * 新增
@@ -225,7 +233,31 @@ export default defineComponent({
       });
     };
 
+    const level1 = ref();
+    /**
+     * 查询所有分类
+     **/
+    const handleQueryCategory = () => {
+      loading.value = true;
+      axios.get("/category/all").then((response) => {
+        loading.value = false;
+        const data = response.data;
+        if (data.success) {
+          const categories = data.content;
+          console.log("原始数组：",categories)
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(categories,0);
+          console.log("树形结构",level1.value);
+
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
     onMounted(() => {
+      handleQueryCategory();
       handleQuery({
         page: 1,
         size: pagination.value.pageSize
@@ -248,7 +280,9 @@ export default defineComponent({
       ebook,
       modalVisible,
       modalLoading,
-      handleModalOk
+      handleModalOk,
+      categoryIds,
+      level1
     }
   }
 })
