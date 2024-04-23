924820ac24d54a0df24e1b34aeb5f646af3a50f6
liuqu <1776540288@qq.com>
2024-04-23 15:21:10 +0800

    8-3 从分类管理复制出文档管理的前后端代码 遗留问题：编辑表中，只能选择一级文档作为父文档

---
+++ src/main/java/com/example/wiki/req/DocQueryReq.java
@@ -0,0 +1,9 @@
+package com.example.wiki.req;
+
+public class DocQueryReq extends PageReq{
+
+    @Override
+    public String toString() {
+        return "DocQueryReq{} " + super.toString();
+    }
+}
---
+++ src/main/java/com/example/wiki/service/DocService.java
@@ -0,0 +1,98 @@
+package com.example.wiki.service;
+
+import com.example.wiki.domain.Doc;
+import com.example.wiki.domain.DocExample;
+import com.example.wiki.mapper.DocMapper;
+import com.example.wiki.req.DocQueryReq;
+import com.example.wiki.req.DocSaveReq;
+import com.example.wiki.resp.DocQueryResp;
+import com.example.wiki.resp.PageResp;
+import com.example.wiki.util.CopyUtil;
+import com.example.wiki.util.SnowFlake;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DocService {
+    /**
+     * Resource是JDK自带的
+     * Autowired是Spring自带的
+     * 两者都可以
+     */
+    @Resource
+    private DocMapper docMapper;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    private static final Logger LOG = LoggerFactory.getLogger(DocService.class);
+
+    public PageResp<DocQueryResp> list(DocQueryReq req){
+        DocExample docExample = new DocExample();
+        docExample.setOrderByClause("sort asc");
+        // 注意：pageNum是从1开始的，另外这个语句只对遇到的第一个select语句起作用，所以保险起见就写在一块
+        PageHelper.startPage(req.getPage(),req.getSize());
+        List<Doc> docList = docMapper.selectByExample(docExample);
+
+        PageInfo<Doc> pageInfo = new PageInfo<>(docList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}",pageInfo.getPages());
+
+//        List<DocResp> respList = new ArrayList<>();
+//        for (Doc doc : docList) {
+////            DocResp docResp = new DocResp();
+////            BeanUtils.copyProperties(doc,docResp);
+//            DocResp docResp = CopyUtil.copy(doc,DocResp.class);  //对象复制
+//            respList.add(docResp);
+//        }
+
+        List<DocQueryResp> respList = CopyUtil.copyList(docList, DocQueryResp.class); //列表复制
+
+        PageResp<DocQueryResp> pageResp = new PageResp<>();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(respList);
+
+        return pageResp;
+    }
+
+    public List<DocQueryResp> all(){
+        DocExample docExample = new DocExample();
+        DocExample.Criteria criteria = docExample.createCriteria();
+        docExample.setOrderByClause("sort asc");
+        List<Doc> docList = docMapper.selectByExample(docExample);
+
+        // 列表复制
+        List<DocQueryResp> respList = CopyUtil.copyList(docList, DocQueryResp.class); //列表复制
+
+        return respList;
+    }
+
+    /**
+     * 保存
+     */
+    public void save(DocSaveReq req){
+        Doc doc = CopyUtil.copy(req, Doc.class);
+        if(ObjectUtils.isEmpty(req.getId())){
+            // 新增
+            doc.setId(snowFlake.nextId());
+            docMapper.insert(doc);
+        }else{
+            // 更新
+            docMapper.updateByPrimaryKey(doc);
+        }
+
+    }
+
+    public void delete(Long id){
+        docMapper.deleteByPrimaryKey(id);
+    }
+
+
+}
