993b9dff8b0e5d51eec3c1f47258401dd228fc52
liuqu <1776540288@qq.com>
2024-04-27 15:28:46 +0800

    8-6 删除某个文档时，增加二次确认 修复bug:ant-design-vue的版本2.2.8->4.1.2,之前误将版本4.1.2安装在了web项目的外面

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
+import 'ant-design-vue/dist/reset.css';
 import * as Icons from '@ant-design/icons-vue';
 import axios from 'axios';
 
--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -5,12 +5,12 @@
         <p>
           <a-form layout="inline" :model="param">
             <a-form-item>
-              <a-button type="primary" @click="handleQuery()" >
+              <a-button type="primary" @click="handleQuery()">
                 查询
               </a-button>
             </a-form-item>
             <a-form-item>
-              <a-button type="primary" @click="add()" >
+              <a-button type="primary" @click="add()">
                 新增
               </a-button>
             </a-form-item>
@@ -23,7 +23,7 @@
             :loading="loading"
             :pagination="false"
         >
-          <template #cover="{ text: cover }" >
+          <template #cover="{ text: cover }">
             <img v-if="cover" :src="cover" alt="avatar" style="width:40px;height:40px"/>
           </template>
           <template v-slot:action="{ text, record }">
@@ -56,7 +56,7 @@
   >
     <a-form :model="doc" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
       <a-form-item label="名称">
-        <a-input v-model:value="doc.name" />
+        <a-input v-model:value="doc.name"/>
       </a-form-item>
       <a-form-item label="父文档">
         <a-tree-select
@@ -71,18 +71,19 @@
         </a-tree-select>
       </a-form-item>
       <a-form-item label="顺序">
-        <a-input v-model:value="doc.sort" />
+        <a-input v-model:value="doc.sort"/>
       </a-form-item>
     </a-form>
   </a-modal>
 </template>
 
 <script lang="ts">
-import {defineComponent, onMounted, reactive, ref, UnwrapRef} from 'vue';
+import {createVNode, defineComponent, onMounted, reactive, ref, UnwrapRef} from 'vue';
 import axios from 'axios';
-import {message} from 'ant-design-vue';
+import {message, Modal} from 'ant-design-vue';
 import {Tool} from '@/util/tool';
 import {useRoute} from "vue-router";
+import {ExclamationCircleOutlined} from '@ant-design/icons-vue';
 
 export default defineComponent({
   name: 'AdminDoc',
@@ -143,11 +144,11 @@ export default defineComponent({
         const data = response.data;
         if (data.success) {
           docs.value = data.content;
-          console.log("原始数组：",docs.value)
+          console.log("原始数组：", docs.value)
 
           level1.value = [];
-          level1.value = Tool.array2Tree(docs.value,0);
-          console.log("树形结构",level1.value);
+          level1.value = Tool.array2Tree(docs.value, 0);
+          console.log("树形结构", level1.value);
 
         } else {
           message.error(data.message);
@@ -165,10 +166,10 @@ export default defineComponent({
 
     const handleModalOk = () => {
       modalLoading.value = true;
-      axios.post("/doc/save",doc.value).then((response) => {
+      axios.post("/doc/save", doc.value).then((response) => {
         modalLoading.value = false;
         const data = response.data; // data = commonResp
-        if(data.success){
+        if (data.success) {
           modalVisible.value = false;
 
           // 重新加载列表
@@ -182,107 +183,118 @@ export default defineComponent({
     /**
      * 将某节点及其子孙节点全部置为disabled
      */
-    const setDisable = (treeSelectData:any,id:any) =>{
-        // console.log(treeSelectData, id);
-        // 遍历数组，即遍历某一层节点
-        for(let i=0;i<treeSelectData.length;i++){
-          const node = treeSelectData[i];
-          if(node.id === id){
-            // 如果当前节点就是目标节点
-            console.log("disable", node);
-            // 将目标节点设置为disabled
-            node.disabled = true;
+    const setDisable = (treeSelectData: any, id: any) => {
+      // console.log(treeSelectData, id);
+      // 遍历数组，即遍历某一层节点
+      for (let i = 0; i < treeSelectData.length; i++) {
+        const node = treeSelectData[i];
+        if (node.id === id) {
+          // 如果当前节点就是目标节点
+          console.log("disable", node);
+          // 将目标节点设置为disabled
+          node.disabled = true;
 
-            // 遍历所有子节点，将所有子节点全部都加上disabled
-            const children = node.children;
-            if(Tool.isNotEmpty(children)){
-              for(let j=0 ; j<children.length;j++){
-                setDisable(children,children[j].id)
-              }
-            }
-          }else{
-            // 如果当前节点不是目标节点，则到其子节点再找找看。
-            const children = node.children;
-            if(Tool.isNotEmpty(children)){
-              setDisable(children,id);
+          // 遍历所有子节点，将所有子节点全部都加上disabled
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            for (let j = 0; j < children.length; j++) {
+              setDisable(children, children[j].id)
             }
           }
+        } else {
+          // 如果当前节点不是目标节点，则到其子节点再找找看。
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            setDisable(children, id);
+          }
         }
+      }
     }
 
     const deleteIds: Array<string> = [];
+    const deleteNames: Array<string> = [];
     /**
      * 查找整根树枝，获取某节点及其子孙节点的ID，用于删除
      */
-    const getDeleteIds = (treeSelectData:any,id:any) =>{
-        // console.log(treeSelectData, id);
-        // 遍历数组，即遍历某一层节点
-        for(let i=0;i<treeSelectData.length;i++){
-          const node = treeSelectData[i];
-          if(node.id === id){
-            // 如果当前节点就是目标节点
-            console.log("delete", node);
-            // 将目标节点的id加入ids
-            deleteIds.push(id);
+    const getDeleteIds = (treeSelectData: any, id: any) => {
+      // console.log(treeSelectData, id);
+      // 遍历数组，即遍历某一层节点
+      for (let i = 0; i < treeSelectData.length; i++) {
+        const node = treeSelectData[i];
+        if (node.id === id) {
+          // 如果当前节点就是目标节点
+          console.log("delete", node);
+          // 将目标节点的id加入ids
+          deleteIds.push(id);
+          deleteNames.push(node.name);
 
-            // 遍历所有子节点
-            const children = node.children;
-            if(Tool.isNotEmpty(children)){
-              for(let j=0 ; j<children.length;j++){
-                getDeleteIds(children,children[j].id)
-              }
-            }
-          }else{
-            // 如果当前节点不是目标节点，则到其子节点再找找看。
-            const children = node.children;
-            if(Tool.isNotEmpty(children)){
-              getDeleteIds(children,id);
+          // 遍历所有子节点
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            for (let j = 0; j < children.length; j++) {
+              getDeleteIds(children, children[j].id)
             }
           }
+        } else {
+          // 如果当前节点不是目标节点，则到其子节点再找找看。
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            getDeleteIds(children, id);
+          }
         }
+      }
     }
 
     /**
      * 编辑
      */
-    const edit = (record:any)=>{
+    const edit = (record: any) => {
       modalVisible.value = true;
       doc.value = Tool.copy(record);
 
       // 不能选择当前节点及其所有子孙节点，作为父节点，会使树断开
       treeSelectData.value = Tool.copy(level1.value);
-      setDisable(treeSelectData.value,record.id);
+      setDisable(treeSelectData.value, record.id);
 
       // 为选择树添加一个”无“
-      treeSelectData.value.unshift({id:0, name:'无'});
+      treeSelectData.value.unshift({id: 0, name: '无'});
     };
 
     /**
      * 新增
      */
-    const add =()=>{
+    const add = () => {
       modalVisible.value = true;
       doc.value = {
-        ebookId:route.query.ebookId
+        ebookId: route.query.ebookId
       };
 
       treeSelectData.value = Tool.copy(level1.value);
 
       // 为选择树添加一个”无“
-      treeSelectData.value.unshift({id:0, name:'无'});
+      treeSelectData.value.unshift({id: 0, name: '无'});
     };
 
-    const handleDelete = (id:number) => {
+    const handleDelete = (id: number) => {
       // 清空数组，否则多次删除时，数组会一直增加
       deleteIds.length = 0;
-      getDeleteIds(level1.value,id);
-      axios.delete("/doc/delete/" + deleteIds.join(",")).then((response) => {
-        const data = response.data; // data = commonResp
-        if(data.success){
-          // 重新加载列表
-          handleQuery();
-        }
-
+      deleteNames.length = 0;
+      getDeleteIds(level1.value, id);
+      Modal.confirm({
+        title: '重要提醒',
+        icon: createVNode(ExclamationCircleOutlined),
+        content: '将删除：【' + deleteNames.join("，") + "】删除后不可恢复，确认删除？",
+        onOk() {
+          axios.delete("/doc/delete/" + deleteIds.join(",")).then((response) => {
+            const data = response.data; // data = commonResp
+            if (data.success) {
+              // 重新加载列表
+              handleQuery();
+            } else {
+              message.error(data.message);
+            }
+          });
+        },
       });
     };
 
