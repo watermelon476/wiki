37d14b3baf67abd46cae9e4b96e4027b9ecaa3cf
liuqu <1776540288@qq.com>
2024-09-02 15:08:11 +0800

    8-12 打开文档页面时，默认选中第一个节点

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -10,6 +10,7 @@
                 @select="onSelect"
                 :replaceFields="{title:'name',key:'id',value:'id'}"
                 :defaultExpandAll="true"
+                :defaultSelectedKeys="defaultSelectedKeys"
             >
             </a-tree>
           </a-col>
@@ -36,6 +37,8 @@ export default defineComponent({
     const route = useRoute()
     const html = ref();
     const docs = ref();
+    const defaultSelectedKeys = ref();
+    defaultSelectedKeys.value = [];
 
     /**
      * 一级文档树，children属性就是二级文档
@@ -50,25 +53,7 @@ export default defineComponent({
      */
     const level1 = ref(); //一级文档树，children属性就是二级文档
     level1.value = [];
-    /**
-     * 数据查询
-     **/
-    const handleQuery = () => {
-      axios.get("/doc/all/" + route.query.ebookId).then((response) => {
-        const data = response.data;
-        if (data.success) {
-          docs.value = data.content;
-          console.log("原始数组：", docs.value)
 
-          level1.value = [];
-          level1.value = Tool.array2Tree(docs.value, 0);
-          console.log("树形结构", level1.value);
-
-        } else {
-          message.error(data.message);
-        }
-      });
-    };
     /**
      * 内容查询
      **/
@@ -82,6 +67,32 @@ export default defineComponent({
         }
       });
     };
+
+    /**
+     * 数据查询
+     **/
+    const handleQuery = () => {
+      axios.get("/doc/all/" + route.query.ebookId).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          docs.value = data.content;
+          // console.log("原始数组：", docs.value);
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(docs.value, 0);
+          // console.log("树形结构", level1.value);
+
+          if(Tool.isNotEmpty(level1)){
+            defaultSelectedKeys.value = [level1.value[0].id];
+            handleQueryContent(level1.value[0].id);
+          }
+
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
 
     const onSelect = (selectedKeys:any,info:any)=>{
       console.log('selected',selectedKeys,info);
