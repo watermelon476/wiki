ccf8e86e660291b64cf30e51c8bcfd825ea36516
liuqu <1776540288@qq.com>
2024-09-13 23:13:54 +0800

    9-8 前后端调试成功，完成基本的登录功能

--- src/main/java/com/example/wiki/req/UserLoginReq.java
+++ src/main/java/com/example/wiki/req/UserLoginReq.java
@@ -1,16 +1,17 @@
 package com.example.wiki.req;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 
 public class UserLoginReq {
 
-    @NotNull(message = "【用户名】不能为空")
+    @NotEmpty(message = "【用户名】不能为空")
     private String loginName;
 
 
-    @NotNull(message = "【密码】不能为空")
+    @NotEmpty(message = "【密码】不能为空")
     // 注意：前端经过加密后，这里的校验其实没有意义了，应该在前端加密码的正则校验
     // @Length(min = 6, max = 20, message = "【密码】6~20位")
     @Pattern(regexp = "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,32}$", message = "【密码】规则不正确")
