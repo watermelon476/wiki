1b24c30c6e1a55545b75fde23ba5e0b5fdb081b1
liuqu <1776540288@qq.com>
2024-09-14 16:39:35 +0800

    9-9 使用vuex保存用户信息，方便其他组件获取登录信息。遗留问题，刷新后值会没有

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,13 +1,21 @@
 <template>
   <a-layout-footer style="text-align: center">
-    Ant Design ©2018 Created by Ant UED 1
+    wiki知识库<span v-show="Boolean(user.id)"></span>，欢迎：{{user.name}}
   </a-layout-footer>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import store from '@/store';
+import {computed, defineComponent} from 'vue';
 
 export default defineComponent({
-  name: 'the-footer'
+  name: 'the-footer',
+  setup(){
+    const user = computed(()=>store.state.user)
+
+    return {
+      user
+    }
+  }
 });
 </script>
