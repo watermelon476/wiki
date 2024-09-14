46289a1884b89b199d0840fe667754a4f892926f
liuqu <1776540288@qq.com>
2024-09-14 15:20:11 +0800

    9-9 集成redis，登录成功后将登录信息放入redis中

--- src/main/java/com/example/wiki/resp/UserLoginResp.java
+++ src/main/java/com/example/wiki/resp/UserLoginResp.java
@@ -13,6 +13,8 @@ public class UserLoginResp {
     @NotNull(message = "【昵称】不能为空")
     private String name;
 
+    private String token;
+
     public Long getId() {
         return id;
     }
@@ -37,17 +39,22 @@ public class UserLoginResp {
         this.name = name;
     }
 
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", loginName=").append(loginName);
-        sb.append(", name=").append(name);
-        sb.append("]");
+        final StringBuilder sb = new StringBuilder("UserLoginResp{");
+        sb.append("id=").append(id);
+        sb.append(", loginName='").append(loginName).append('\'');
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", token='").append(token).append('\'');
+        sb.append('}');
         return sb.toString();
     }
 }
