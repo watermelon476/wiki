ac34cffde9bf965ce443906d55c1b5c23930b0cd
liuqu <1776540288@qq.com>
2024-10-27 13:52:21 +0800

    10-4 定时任务示例

--- src/main/java/com/example/wiki/config/WikiApplication.java
+++ src/main/java/com/example/wiki/config/WikiApplication.java
@@ -7,11 +7,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 //@ComponentScan({"com.example.wiki","com.test"}) //扫描多个包的写法
 @ComponentScan("com.example.wiki")
 @SpringBootApplication
 @MapperScan("com.example.wiki.mapper")
+@EnableScheduling
 public class WikiApplication {
 
 	private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
