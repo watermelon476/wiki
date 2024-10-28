e00ec30aa96bc8eaf2a3eea863e44098986a397f
liuqu <1776540288@qq.com>
2024-10-29 00:59:31 +0800

    10-6 增加定时器，定时执行电子书信息更新SQL

--- src/main/java/com/example/wiki/mapper/DocMapperCust.java
+++ src/main/java/com/example/wiki/mapper/DocMapperCust.java
@@ -10,4 +10,6 @@ public interface DocMapperCust {
     public void increaseViewCount(@Param("id") Long id);
 
     public void increaseVoteCount(@Param("id") Long id);
+
+    public void updateEbookInfo();
 }
