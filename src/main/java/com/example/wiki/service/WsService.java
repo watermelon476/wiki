b38a7baae6db9e28c06e8f824a59b2be95758b5e
liuqu <1776540288@qq.com>
2024-11-22 16:59:37 +0800

    10-10 将业务线程日志流水号传递到异步线程，方便生产运维查问题

--- src/main/java/com/example/wiki/service/WsService.java
+++ src/main/java/com/example/wiki/service/WsService.java
@@ -2,6 +2,7 @@ package com.example.wiki.service;
 
 import com.example.wiki.domain.Doc;
 import com.example.wiki.websocket.WebSocketServer;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +14,8 @@ public class WsService {
     public WebSocketServer webSocketServer;
 
     @Async
-    public void sendInfo(String message){
-
+    public void sendInfo(String message,String logId){
+        MDC.put("LOG_ID", logId);
         webSocketServer.sendInfo(message);
     }
 }
