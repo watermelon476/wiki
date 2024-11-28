58ea901dfde6c05e0151f58a6ac4bf695ef92d1e
liuqu <1776540288@qq.com>
2024-11-29 00:30:20 +0800

    10-11 使用RocketMQ解耦点赞通知功能，点赞->发MQ->收MQ->推送WS消息->弹出通知

--- src/main/java/com/example/wiki/rocketmq/VoteTopicConsumer.java
+++ src/main/java/com/example/wiki/rocketmq/VoteTopicConsumer.java
@@ -16,9 +16,13 @@ public class VoteTopicConsumer implements RocketMQListener<MessageExt> {
 
     private static final Logger LOG = LoggerFactory.getLogger(VoteTopicConsumer.class);
 
+    @Resource
+    public WebSocketServer webSocketServer;
+
     @Override
     public void onMessage(MessageExt messageExt) {
         byte[] body = messageExt.getBody();
         LOG.info("ROCKETMQ收到消息：{}", new String(body));
+        webSocketServer.sendInfo(new String(body));
     }
 }
